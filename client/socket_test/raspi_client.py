fd93a3ffe666f08f2f99f400e58f82e34aac2145
Xu, Hao <hao.xu@wur.nl>
2023-07-20 14:54:40 +0800

    update raspi test code

--- client/socket_test/raspi_client.py
+++ client/socket_test/raspi_client.py
@@ -1,13 +1,40 @@
+#import necessary package
 import socket
-import struct
-
-HOST = '192.168.1.103'
-PORT = 8888
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.connect((HOST, PORT))
-
+import time
+import sys
+import RPi.GPIO as GPIO
+#define host ip: Rpi's IP
+HOST_IP = "192.168.1.101"
+HOST_PORT = 8888
+print("Starting socket: TCP...")
+#1.create socket object:socket=socket.socket(family,type)
+socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("TCP server listen @ %s:%d!" %(HOST_IP, HOST_PORT) )
+host_addr = (HOST_IP, HOST_PORT)
+#2.bind socket to addr:socket.bind(address)
+socket_tcp.bind(host_addr)
+#3.listen connection request:socket.listen(backlog)
+socket_tcp.listen(1)
+#4.waite for client:connection,address=socket.accept()
+socket_con, (client_ip, client_port) = socket_tcp.accept()
+print("Connection accepted from %s." %client_ip)
+socket_con.send("Welcome to RPi TCP server!".encode())
+#5.handle
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+print("Receiving package...")
 while True:
-    sock.sendall(struct.pack('i', 1))
-
-sock.close()
+    try:
+        data=socket_con.recv(512)
+        if len(data)>0:
+            print("Received:%s"%data)
+            if data=='1':
+                GPIO.output(11,GPIO.HIGH)
+            elif data=='0':
+                GPIO.output(11,GPIO.LOW)
+            socket_con.send(data)
+            time.sleep(1)
+            continue
+    except Exception:
+            socket_tcp.close()
+            sys.exit(1)
