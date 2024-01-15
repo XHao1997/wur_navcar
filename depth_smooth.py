e381f80a5a92ff4d565becbc13a70af6959895f1
Hao Xu <xuhao6815@gmail.com>
2024-01-15 20:36:07 +0800

    add requirement list

--- depth_smooth.py
+++ depth_smooth.py
@@ -9,9 +9,9 @@ from kinect_smoothing import Crop_Filter, Smooth_Filter, Motion_Sampler
 from kinect_smoothing.utils import plot_image_frame
 
 def standard_pipeline(image_frame):
-    hole_filter = HoleFilling_Filter(flag='min', min_valid_depth=150)
+    hole_filter = HoleFilling_Filter(flag='mode', min_valid_depth=50, min_valid_neighbors=5,radius=0.2)
     image_frame = hole_filter.smooth_image(image_frame)
-    hole_filter = HoleFilling_Filter(flag='ns', min_valid_depth=0, max_valid_depth=255., radius=20)
+    hole_filter = HoleFilling_Filter(flag='ns', min_valid_depth=0, max_valid_depth=255., radius=1)
     image_frame = hole_filter.smooth_image(image_frame)
     noise_filter = Denoising_Filter(flag='gaussian')
     image_frame = noise_filter.smooth_image(image_frame)
@@ -25,6 +25,8 @@ def kinect_preprocess(img_path):
     # print(image_frame.shape)
     image_frame = standard_pipeline(image_frame)
 
+
+    print('preprocessed image %s, time-cost %f s' % (img_path, time() - t1))
     while (1):
         cv2.imshow('img', image_frame)
         k = cv2.waitKey(33)
@@ -36,8 +38,6 @@ def kinect_preprocess(img_path):
             print
             k  # else print its value
     # cv2.imshow('image', image_frame)
-    print('preprocessed image %s, time-cost %f s' % (img_path, time() - t1))
-
 
 if __name__ == '__main__':
     kinect_preprocess('data/depth.png')
