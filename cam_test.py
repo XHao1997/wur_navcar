7d6e24a6bddbad95171482cc6eae368449a8df50
Hao Xu <xuhao6815@gmail.com>
2024-04-25 17:47:43 +0200

    update joint1 training set

--- cam_test.py
+++ cam_test.py
@@ -9,25 +9,26 @@ if __name__ == "__main__":
     block_center = []
     rgb_img = kinect.capture_rgb_img()
     print('rgb image captured')
-    for i in range(10):
+    for i in range(1):
         depth_img = kinect.capture_depth_img() 
         print('depth image captured')
         center = kinect.execute_task(rgb_img,depth_img,'block',debug=False)
         block_center.append(np.round(center))
     print(np.median(block_center,axis=0))
     
-    for i in range(10):
-        time.sleep(.1)
+    
+    # for i in range(10):
+    #     time.sleep(.1)
 
-        depth_img = kinect.capture_depth_img() 
-        print('depth image captured')
-        center = kinect.execute_task(rgb_img,depth_img,'block',debug=False)
-        block_center.append(np.round(center))
-    print(np.median(block_center,axis=0))
-    for i in range(10):
-        time.sleep(.1)
-        depth_img = kinect.capture_depth_img() 
-        print('depth image captured')
-        center = kinect.execute_task(rgb_img,depth_img,'block',debug=False)
-        block_center.append(np.round(center))
-    print(np.median(block_center,axis=0))
+    #     depth_img = kinect.capture_depth_img() 
+    #     print('depth image captured')
+    #     center = kinect.execute_task(rgb_img,depth_img,'block',debug=False)
+    #     block_center.append(np.round(center))
+    # print(np.median(block_center,axis=0))
+    # for i in range(10):
+    #     time.sleep(.1)
+    #     depth_img = kinect.capture_depth_img() 
+    #     print('depth image captured')
+    #     center = kinect.execute_task(rgb_img,depth_img,'block',debug=False)
+    #     block_center.append(np.round(center))
+    # print(np.median(block_center,axis=0))
